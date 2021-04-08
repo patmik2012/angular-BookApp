0932702c1dd69f1e9fa19bb9217e7ce13f1de4e6
patmik2012 <pataki.miklos@gmail.com>
2021-04-08 23:29:52 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,43 +7,12 @@ import { mergeMap, map,startWith,debounceTime } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
-books=
-[
-    {
-      "id": 1,
-      "title": "Első könyv",
-      "authorId": 1,
-      "published": 2011,
-      "pages": 345,
-      "ISBN": "2352145234",
-      "likes":32
-    },
-    {
-      "id": 2,
-      "title": "Második könyv",
-      "authorId": 1,
-      "published": 2010,
-      "pages": 532,
-      "ISBN": "12355326312",
-      "likes":23
-    },
-    {
-      "id": 3,
-      "title": "Harmadik könyv",
-      "authorId": 2,
-      "published": 2003, 
-      "pages": 147,
-      "ISBN": "635234265623",
-      "likes":12
-    }
-  ]
 
   name = 'BookApp';
 
-  liked($event){
-    alert($event);
-  }
+
 
 
 }
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -7,10 +7,44 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-
+books=
+[
+    {
+      "id": 1,
+      "title": "Első könyv",
+      "authorId": 1,
+      "published": 2011,
+      "pages": 345,
+      "ISBN": "2352145234",
+      "likes":32
+    },
+    {
+      "id": 2,
+      "title": "Második könyv",
+      "authorId": 1,
+      "published": 2010,
+      "pages": 532,
+      "ISBN": "12355326312",
+      "likes":23
+    },
+    {
+      "id": 3,
+      "title": "Harmadik könyv",
+      "authorId": 2,
+      "published": 2003, 
+      "pages": 147,
+      "ISBN": "635234265623",
+      "likes":12
+    }
+  ]
+  
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  liked($event){
+    alert($event);
   }
 
 }
