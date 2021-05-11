8fc17ab2ac7594ce0751d2da1e3210b5ecf78740
patmik2012 <pataki.miklos@gmail.com>
2021-05-11 22:26:32 +0200

    4 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,27 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<mat-card><h1>{{name}}</h1></mat-card>`,
+  template: `
+  <mat-card>
+    <h1>Welcome in {{name}}!</h1>
+    <nav>
+      <ul>
+        <li><a mat-button routerLink="/books">
+            <b>Books</b>
+          </a></li>
+      </ul>
+      <ul>
+        <li><a mat-button routerLink="/authors">
+            <b>Authors</b>
+          </a></li>
+      </ul>
+      <ul>
+        <li><a mat-button routerLink="/bookstores">
+            <b>Bookstores</b>
+          </a></li>
+      </ul>
+    </nav>
+  </mat-card>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
